d9ef2f3a19051f655f3e709f4cd2cbd277512f13
Subin <subinsamrat96@gmail.com>
2020-03-22 15:50:31 +0530

    404 page design completed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import {Link} from 'gatsby'
 import Layout from '../components/Layout'
+import styles from '../css/error.module.css'
+import Banner from '../components/Banner'
 
-export default function error() {
+export default function error({title}) {
   return(
     <Layout>
-      Oops! It's a dead end.
-      <div>
-        <button>
-          <Link to="/">Back to home</Link>
-        </button>
-      </div>
-    
+      <header className={styles.error}>
+        <Banner title="Oops! It's a dead end.">
+          <Link to="/" className="btn-white">Back to home</Link>
+        </Banner>
+      </header>
     </Layout>
   ) 
 }
