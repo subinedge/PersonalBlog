1c76d90bc1d1836bc269cccb38ea43db1f597564
Subin <subinsamrat96@gmail.com>
2020-03-22 15:36:40 +0530

    landing page design completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
 import React from "react"
+import {Link} from 'gatsby'
 import Layout from '../components/Layout'
+import SimpleHero from '../components/Simplehero'
+import Banner from '../components/Banner'
 
 export default () => { 
   return (
   <Layout>
-    Hello world!
+    <SimpleHero>
+      <Banner title="FrontEndDev Blog" info="Let us take you into a deeper experience, make a moment a lasting conveyable memory. Let us help build your tribe.">
+      <Link to="/blog" className="btn-white">
+        Learn More
+      </Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
   )
 }
